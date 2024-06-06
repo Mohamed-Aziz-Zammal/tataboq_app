1befbd914adad203d9971f5cd9555d1d0f5dddfb
mohamed-aziz <medazizzammal@gmail.com>
2024-06-06 22:58:48 +0100

    v 1

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Text('تطابق صورة'),
+        backgroundColor: Colors.indigo[800],
+      ),
+      body: ImagePage(),
+    ),
+  ));
+}
+
+class ImagePage extends StatelessWidget {
+  const ImagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
